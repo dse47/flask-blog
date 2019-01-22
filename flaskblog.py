29def357a948e083548a6e6b4167d9b1f36139c4
Dong-Seop Eah <deah@bu.edu>
2019-01-22 18:31:35 -0500

    create forms.py

--- flaskblog.py
+++ flaskblog.py
@@ -12,7 +12,7 @@ posts = [
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'April 21, 2018' 
     }
 ]
 
